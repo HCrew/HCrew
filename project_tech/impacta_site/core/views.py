d5d4cddb02235c9def20aa477a259afc2a85d46f
Petterson <Pettersonp@live.com>
2018-03-16 21:28:54 -0300

    utilização da função render nas views

--- project_tech/impacta_site/core/views.py
+++ project_tech/impacta_site/core/views.py
@@ -1,29 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django import forms
 
 
 # Create your views here.
 
 # Função para retornar view de index.html
-
 def index(request):
-    template = loader.get_template('core/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'core/index.html')
 
 
 # Função para retornar view de contato.html
-
 def contato(request):
-    template = loader.get_template('core/contato.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'core/contato.html')
+
 
 # Função para retornar view de login.html
-
 def login(request):
-    template = loader.get_template('core/login.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'core/login.html')
