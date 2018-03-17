9c5394f49c2c67fc358efaa849d090b31e9edb12
Petterson <Pettersonp@live.com>
2018-03-16 21:26:42 -0300

    ajuste das urls fazendo uso de namespaces e names

--- project_tech/impacta_site/core/urls.py
+++ project_tech/impacta_site/core/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-
-url('', views.index, name='index'),
-
-
+    path('', views.index, name='index'),
+    path('contato/', views.contato, name='contato'),
+    path('login/', views.login, name='login'),
 ]
--- project_tech/impacta_site/impacta_site/urls.py
+++ project_tech/impacta_site/impacta_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import core.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
+    path('', include((core.urls.urlpatterns, 'core'))),
 ]
