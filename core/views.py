500ebfce17b0220156c326626b61c3357c645aa3
Petterson <Pettersonp@live.com>
2018-03-19 15:24:19 -0300

    updated login form, post and get method defined in login and contato views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -13,9 +13,18 @@ def index(request):
 
 # Função para retornar view de contato.html
 def contato(request):
-    return render(request, 'core/contato.html')
+    if request.method == 'GET':
+        return render(request, 'core/contato.html')
+    elif request.method == 'POST':
+        print(request.POST)
+        return redirect('/contato')
 
 
 # Função para retornar view de login.html
 def login(request):
-    return render(request, 'core/login.html')
+    
+    if request.method == 'GET':
+        return render(request, 'core/login.html')
+    elif request.method == 'POST':
+        print (request.POST)
+        return redirect('/')
