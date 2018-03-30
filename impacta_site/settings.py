0cc9aff0053f53ceddfbf2bbdfbfd52fb7d32ec5
Fawers <fabricioswerneck@gmail.com>
2018-03-30 17:33:15 -0300

    add STATICFILES_DIRS to settings.py
    this way, the app 'staticfiles' knows where to look for static files, like css, js, and images.

--- impacta_site/settings.py
+++ impacta_site/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
@@ -120,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
