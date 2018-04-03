50aa0a7e1a3963f77683bf7c111cb3bf2e5be82c
Fabricio Werneck <fabricio.werneck@dafiti.com.br>
2018-04-03 20:39:33 -0300

    add logout url path

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contato/', views.contato, name='contato'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('cursos/', views.cursos, name='cursos'),
     path('form_novo_curso/', views.form_novo_curso, name='form_novo_curso'),
     path('disciplina_cursos/', views.disciplina_cursos, name='disciplina_cursos'),
