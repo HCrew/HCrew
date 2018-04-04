f6be58fc9c1eb20a0494df463643d948b7a3ad88
matheussilva97 <matheus.alcantara97@hotmail.com>
2018-04-04 09:36:01 -0300

    correcao do metodo

--- core/urls.py
+++ core/urls.py
@@ -13,3 +13,4 @@ urlpatterns = [
     #path('subjects/<subject_name>/', views.subject_detail, name='subject_detail'),
     path('detalhesCurso/', views.detalhesCurso, name='detalhesCurso'),
     path('form_nova_disciplina/', views.form_nova_disciplina, name='form_nova_disciplina')
+]
