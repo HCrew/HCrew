108d363cdf15b2e759a664b0d5cc8b2a72dae388
Abel Separovich <separovi@gmail.com>
2018-05-03 21:38:56 -0300

    retirando models.py e começando AC9 Ponto 3

--- contas/models/aluno.py
+++ contas/models/aluno.py
@@ -1,18 +1,15 @@
 from django.db import models
+from .pessoa import Pessoa
 
-class Aluno(models.Model):
-    id_aluno = models.AutoField(primary_key=True)
-    login_aluno = models.CharField(unique=True, max_length=50)
-    senha_aluno = models.CharField(max_length=70)
-    nome_aluno = models.CharField(max_length=100)
-    email_aluno = models.CharField(unique=True, max_length=70)
-    celular_aluno = models.IntegerField(unique=True)
-    dt_expiracao_aluno = models.DateField(default='1900-01-01')
+class Aluno(Pessoa):
     ra_aluno = models.IntegerField(unique=True)
     foto_aluno = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.nome_aluno
+
+    def retorna_carga_horaria(self):
+        return 'Metodo não implementado' #IMPLEMENTAR ESTE METODO
 
     class Meta:
         managed = False
