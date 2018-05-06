e851e4285fa6aa3e352d3d841f59803b66a0d2fe
Petterson <herbalistp@gmail.com>
2018-05-06 16:51:02 -0300

    finish CRUD Atividade

--- restrito/urls.py
+++ restrito/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import ( pesquisarAtividades, novaAtividade, editarAtividade, excluirAtividade)
 
 urlpatterns = [
-
+path('pesquisarAtividades/', pesquisarAtividades),
+path('novaAtividade/', novaAtividade),
+path('editarAtividade/<int:id>/', editarAtividade),
+path('excluirAtividade/<int:id>/', excluirAtividade)
 ]
